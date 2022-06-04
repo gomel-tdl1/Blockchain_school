37172c50bed1edd2945e5a29e7a6a7e39ecb5d49
gomel-tdl1 <rock_cell@mail.ru>
2022-06-04 11:51:13 +0300

    change contract

--- contracts/simple-ERC20/simpleERC20.sol
+++ contracts/simple-ERC20/simpleERC20.sol
@@ -4,17 +4,28 @@ pragma solidity ^0.8.6;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
+struct Vote {
+    address votingOwner;
+    uint256 votingNumber;
+    uint256 votedAgree;
+    uint256 votedDesagree;
+    uint256 newPrice;
+}
+
 contract SimpleERC20 is ERC20, Ownable {
-    uint64 public timeToVote;
+    uint256 public timeToVote;
     uint256 public price;
+    uint256 public prevVotingNumber;
+    Vote public currentVoting;
 
-    mapping(address => uint256) private _votedPrices;
-    address[] private _voters;
-    uint64 private _startVotingTimestamp;
+    mapping(address => mapping(uint256 => bool)) private _isVoterVoted;
 
-    constructor(uint64 _timeToVote, uint256 _price, uint256 initialSupply, string memory _name, string memory _symbol) public ERC20(_name, _symbol) {
+    uint256 private _startVotingTimestamp;
+
+    constructor(uint256 _timeToVote, uint256 _price, uint256 initialSupply, string memory _name, string memory _symbol) public ERC20(_name, _symbol) {
         timeToVote = _timeToVote;
         price = _price;
+        prevVotingNumber = 0;
         _mint(address(this), initialSupply);
     }
 
@@ -23,48 +34,37 @@ contract SimpleERC20 is ERC20, Ownable {
     }
 
     function _clearVoting() private {
-        for(uint16 i = 0; i < _voters.length; i++) {
-            address voter = _voters[i];
-            _votedPrices[voter] = 0;
-        }
-        _voters = new address[](0);
         _startVotingTimestamp = 0;
     }
 
-    function startVoting() external onlyOwner() {
+    function startVoting(uint256 _price) external {
+        require(_startVotingTimestamp != 0, "voting already started");
+        ++prevVotingNumber;
         _startVotingTimestamp = uint64(block.timestamp);
+        currentVoting = Vote(msg.sender, prevVotingNumber, 0, 0, _price);
     }
 
-    function votePriceChange(uint256 _price) external {
-        require(_price > 0, "price = 0");
+    function votePriceChange(bool _agreement) external {
         require(_startVotingTimestamp == 0, "!voting");
+        require(!_isVoterVoted[msg.sender][currentVoting.votingNumber], "user already voted");
 
         uint256 balancePercent = calculateBalancePercents(balanceOf(msg.sender));
-        uint64 endVotingTimestamp = _startVotingTimestamp + timeToVote;
+        uint256 endVotingTimestamp = _startVotingTimestamp + timeToVote;
 
         require(balancePercent >= 500, "balance percent < 5%");
-        require(endVotingTimestamp > uint64(block.timestamp) && _startVotingTimestamp < uint64(block.timestamp), "!voting" );
+        require(endVotingTimestamp > block.timestamp && _startVotingTimestamp < block.timestamp, "!voting" );
 
-        _votedPrices[msg.sender] = _price;
-        _voters.push(msg.sender);
+        if(_agreement) currentVoting.votedAgree++;
+        if(!_agreement) currentVoting.votedDesagree++;
+        _isVoterVoted[msg.sender][currentVoting.votingNumber] = true;
     }
 
-    function changePriceAndClearVoting() external onlyOwner(){
+    function changePriceAndClearVoting() external {
         require(_startVotingTimestamp + timeToVote < uint64(block.timestamp), "!end");
 
-        address highestHolder;
-
-        uint256 highestBalance = 0;
-        for(uint16 i = 0; i < _voters.length; i++) {
-            address voter = _voters[i];
-            uint256 voterBalance = balanceOf(voter);
-            if(voterBalance > highestBalance){
-                highestBalance = voterBalance;
-                highestHolder = voter;
-            }
+        if(currentVoting.votedAgree > currentVoting.votedDesagree){
+            price = currentVoting.newPrice;
         }
-
-        price = _votedPrices[highestHolder];
 
         _clearVoting();
     }
